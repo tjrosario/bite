2fcbb870383d0f3367d81af57ec67916fe9d96de
Tommy Rosario <tom.jason.rosario@gmail.com>
2024-05-20 02:22:10 -0400

    use vercel prod url

--- utils/item.ts
+++ utils/item.ts
@@ -1,13 +1,18 @@
 import { Item } from '@/types/item';
 
+export const isProduction = process.env.NODE_ENV === 'production';
+const URL = isProduction
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : process.env.URL;
+
 export async function fetchItem(id: string): Promise<Item> {
-  const resp = await fetch(`${process.env.URL}/api/items/${id}`);
+  const resp = await fetch(`${URL}/api/items/${id}`);
   const { data } = await resp.json();
   return data;
 }
 
 export async function fetchItems(): Promise<Item[]> {
-  const resp = await fetch(`${process.env.URL}/api/items`);
+  const resp = await fetch(`${URL}/api/items`);
   const { data } = await resp.json();
   return data;
 }
